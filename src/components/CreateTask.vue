c472c3991418e65699ce83be4829150a4629725a
Moussa Sani <moussa.sani@mh-or.com>
2024-02-05 17:11:42 +0100

    Setup vue app for todos

---
+++ src/components/CreateTask.vue
@@ -0,0 +1,38 @@
+<template>
+    <h3>Create new Todo</h3>
+    <form>
+    <div class="mb-3">
+      <label for="title" class="form-label">Title:</label>
+      <input v-model="title" type="text" class="form-control" id="title" name="title" required>
+    </div>
+    <div class="mb-3">
+      <label for="username" class="form-label">Username:</label>
+      <input v-model="username" type="text" class="form-control" id="username" name="username" required>
+    </div>
+    <div class="mb-3 form-check">
+      <input v-model="isTaskDone" :value="statusChange" type="checkbox" class="form-check-input" id="isTaskDone" name="isTaskDone">
+      <label class="form-check-label" for="isTaskDone">Is Task Done</label>
+    </div>
+    <button v-on:click="$emit('submitForm', [title, username, isTaskDone])" type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: "",
+                username: "",
+                isTaskDone: false,
+            }
+        },
+        watch: {
+            statusChange(status) {
+                this.isTaskDone = status
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container mt-4">
+  <div class="row">
+    <div v-for="(item, index) in todos" :key="index">
+        <div class="col-md-4">
+        <div class="card mb-4">
+          <div class="card-body">
+            <h5 class="card-title">{{ item.title }}</h5>
+            <div>
+                <label>Is task done ?</label>
+                <input type="checkbox" v-model="item.isTaskDone" :value="$emit('toggleTask', [item.id, item.isTaskDone])">
+            </div>
+            <p class="card-text">{{ item.username }}</p>
+            <button class="btn btn-danger" v-on:click="$emit('delete', [item.id])">Delete Task</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                todos: [
+                    {
+                    "id": 1,
+                    "title": "Title 1",
+                    "isTaskDone": true,
+                    "username": "username 1",
+                    },
+                    {
+                    "id": 2,
+                    "title": "Title 2",
+                    "isTaskDone": false,
+                    "username": "username 2",
+                    },
+                    {
+                    "id": 3,
+                    "title": "Title 3",
+                    "isTaskDone": false,
+                    "username": "username 3",
+                    }
+                ]
+            }
+        },
+    }
+</script>
